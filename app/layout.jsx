2825382fa2e8307eae2c0ddc3a823be0f0db3dbe
Shashank Sharma <78837537+theshashankk@users.noreply.github.com>
2025-09-19 23:51:22 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,59 +5,57 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 import "./nprogress.css";
 import { Analytics } from "@vercel/analytics/react";
-import Chat from "@/components/Chat";
 import ClientTopProgressBar from "@/components/ClientTopProgressBar";
 
 export const metadata = {
     title: "Shashank | Portofolio",
 
     description:
-		"I’m Shashank sharma, a BBA student specializing in Digital Marketing at Dev Bhoomi Uttarakhand University.",
+        "I’m Shashank sharma, a BBA student specializing in Digital Marketing at Dev Bhoomi Uttarakhand University.",
 
     author: "Shashank Sharma",
     siteUrl: "https://www.theshashankk.github.io",
     applicationName: "Shashank",
 
     keywords: [
-		"theshashankk",
-		"theshashank",
-		"Shashank sharma",
-		"Shashank",
-		"Shashank sharma bokaro",
-		"shashank sharma dehradun",
-		"bloodfallen",
-		"shashank portfolio",
-		"shashank github",
-	],
+        "theshashankk",
+        "theshashank",
+        "Shashank sharma",
+        "Shashank",
+        "Shashank sharma bokaro",
+        "shashank sharma dehradun",
+        "bloodfallen",
+        "shashank portfolio",
+        "shashank github",
+    ],
 
     openGraph: {
-		type: "website",
-		url: "https://www.theshashankk.github.io",
-		title: "Shashank | Portofolio",
-		site_name: "Shashank | Portofolio",
-		description: "My name is shashank, This is my portofolio website.",
-		width: 1200,
-		height: 630,
-		images: [
-			{
-				url: "/og-image-rev.png",
-				alt: "Shashank Portofolio",
-			},
-		],
-		site_name: "Shashank | Portofolio",
-	}
+        type: "website",
+        url: "https://www.theshashankk.github.io",
+        title: "Shashank | Portofolio",
+        site_name: "Shashank | Portofolio",
+        description: "My name is shashank, This is my portofolio website.",
+        width: 1200,
+        height: 630,
+        images: [
+            {
+                url: "/og-image-rev.png",
+                alt: "Shashank Portofolio",
+            },
+        ],
+        site_name: "Shashank | Portofolio",
+    }
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<body>
-				<ClientTopProgressBar />
-				<Navbar />
-				{children}
-				<Chat />
-				<Analytics />
-			</body>
-		</html>
-	);
+    return (
+        <html lang="en">
+            <body>
+                <ClientTopProgressBar />
+                <Navbar />
+                {children}
+                <Analytics />
+            </body>
+        </html>
+    );
 }
