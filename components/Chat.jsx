96f3eaa6889a122ec1e2e13b81a3f8b5b2201570
Shashank Sharma <78837537+theshashankk@users.noreply.github.com>
2025-09-20 15:31:14 +0530

    KoFi

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -5,15 +5,20 @@ import Script from "next/script";
 const KoFi = () => {
   return (
     <>
-      <script src='https://storage.ko-fi.com/cdn/scripts/overlay-widget.js'></script>
-<script>
-  kofiWidgetOverlay.draw('shashank76914', {
-    'type': 'floating-chat',
-    'floating-chat.donateButton.text': 'Support Me',
-    'floating-chat.donateButton.background-color': '#323842',
-    'floating-chat.donateButton.text-color': '#fff'
-  });
-</script>
+      {/* Load Ko-fi widget script */}
+      <Script src="https://storage.ko-fi.com/cdn/scripts/overlay-widget.js" strategy="afterInteractive" />
+
+      {/* Initialize widget after script loads */}
+      <Script id="ko-fi-widget" strategy="afterInteractive">
+        {`
+          kofiWidgetOverlay.draw('shashank76914', {
+            type: 'floating-chat',
+            'floating-chat.donateButton.text': 'Support Me',
+            'floating-chat.donateButton.background-color': '#323842',
+            'floating-chat.donateButton.text-color': '#fff'
+          });
+        `}
+      </Script>
     </>
   );
 };
