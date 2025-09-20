2898ea40a98de33da3e1ca40e279d7a7271fd0ec
Shashank Sharma <78837537+theshashankk@users.noreply.github.com>
2025-09-20 13:31:57 +0530

    Update generate-sitemap.js

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 
 async function generateSitemap() {
 	const sitemap = new SitemapStream({
-		hostname: "https://theshashankk.github.io/",
+		hostname: "https://theshashank.qzz.io/",
 	});
 
 	// Add URLs to your sitemap
