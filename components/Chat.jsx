b00aa7916856b6fa065092617931392f04017b4d
Shashank Sharma <78837537+theshashankk@users.noreply.github.com>
2025-09-20 09:13:17 +0530

    Update Chat.jsx

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,35 +1,37 @@
-// components/Intelliticks.js
+// components/Chat.js
 "use client"
 import { useEffect } from "react";
 
-// Component for Intelliticks chat widget
 const Chat = () => {
-	useEffect(() => {
-		// Paste the Intelliticks script here
-		(function (I, L, T, i, c, k, s) {
-			if (I.iticks) return;
-			I.iticks = { host: c, settings: s, clientId: k, cdn: L, queue: [] };
-			var h = T.head || T.documentElement;
-			var e = T.createElement(i);
-			var l = I.location;
-			e.async = true;
-			e.src = (L || c) + "/client/inject-v2.min.js";
-			h.insertBefore(e, h.firstChild);
-			I.iticks.call = function (a, b) {
-				I.iticks.queue.push([a, b]);
-			};
-		})(
-			window,
-			"https://cdn-v1.intelliticks.com/prod/common",
-			document,
-			"script",
-			"https://app.intelliticks.com",
-			"u3cWw3sDpdKexqXCY_c",
-			{}
-		);
-	}, []);
+  useEffect(() => {
+    // Add Landbot script
+    function initLandbot() {
+      if (!window.myLandbot) {
+        const s = document.createElement("script");
+        s.type = "module";
+        s.async = true;
+        s.addEventListener("load", function () {
+          window.myLandbot = new window.Landbot.Popup({
+            configUrl:
+              "https://storage.googleapis.com/landbot.online/v3/H-3140987-0BIFL01NTPNYDQ41/index.json",
+          });
+        });
+        s.src = "https://cdn.landbot.io/landbot-3/landbot-3.0.0.mjs";
+        const x = document.getElementsByTagName("script")[0];
+        x.parentNode.insertBefore(s, x);
+      }
+    }
 
-	return null;
+    window.addEventListener("mouseover", initLandbot, { once: true });
+    window.addEventListener("touchstart", initLandbot, { once: true });
+
+    return () => {
+      window.removeEventListener("mouseover", initLandbot);
+      window.removeEventListener("touchstart", initLandbot);
+    };
+  }, []);
+
+  return null;
 };
 
 export default Chat;
